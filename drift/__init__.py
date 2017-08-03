dd652e2856839e7d1cb48d37720cace61e8fa79b
farshid <farshidrayhan57@gmail.com>
2017-08-04 02:03:27 +0600

    updated

--- drift/__init__.py
+++ drift/__init__.py
@@ -2,15 +2,49 @@ import sklearn.datasets
 import sklearn.tree
 import collections
 import numpy
-
+import pandas
 from sklearn.model_selection import train_test_split
 
 clf = sklearn.tree.DecisionTreeClassifier(random_state=42)
 iris = sklearn.datasets.load_iris()
 
+total_matrix = pandas.DataFrame(pandas.read_csv('sea.txt'))
 
-x_train ,  x_test , y_train  ,  y_test=  train_test_split(iris.data,iris.target,test_size=.3)
-# x_test , y_test = iris.data[90:] , iris.target[90:]
+x_train = []
+x_test = []
+y_train = []
+y_test =[]
+
+total_matrix = numpy.ndarray.tolist(pandas.DataFrame.as_matrix(total_matrix))
+count = 0
+X = []
+y = []
+print("Total instances ", len(total_matrix))
+for l in total_matrix:
+    last_index = len(l) - 1
+    X.append(l[0:last_index])
+    y.append(l[last_index])
+    count += 1
+    if count == 1000 :
+        break
+X = iris.data
+y = iris.target
+
+count = 0
+for i in y :
+    if i == 0 or i == 2:
+        x_train.append(X[count])
+        y_train.append(y[count])
+    count += 1
+
+count = 0
+for i in y :
+    if i == 1:
+        x_test.append(X[count])
+        y_test.append(y[count])
+    count += 1
+
+# x_train ,  x_test , y_train  ,  y_test=  train_test_split(X,y,test_size=.3,random_state=13)
 
 number_of_features = len(x_train[0])
 
@@ -29,8 +63,6 @@ for d, dec in enumerate(dec_paths):
 # print(clf.apply(iris.data))
 # tree_structure = clf.tree_.__getstate__()['nodes']
 # print(tree_structure)
-
-counter = 0
 # for i in tree_structure:
 #     print(tree_structure[i][1])
 #     counter += 1
@@ -45,7 +77,7 @@ print('nodes who are leaf ',list( tree_leafs_unique))
 centroid_lst = []
 
 leaf_centroids = {}
-
+print("calculating Centroids ")
 for i in tree_leafs_unique:
     # for j in tree_leafs:
     #     centroid_lst[i].append(x_train[i])
@@ -84,14 +116,50 @@ for i in leaf_centroids:
 
 print()
 print("### Test Phase ###",end='\n\n')
-# clf.fit(x_test, y_test)
+
+leaf_centroids_test = {}
 tree_leafs_unique = list(set(clf.apply(x_test)))   # leafs contains the leaf nodes of the tree
 tree_leafs = clf.apply(x_test)   # leafs contains the leaf nodes of the tree
-print(tree_leafs)
+# print(tree_leafs)
 
 # for i in tree_leafs:
 #     print(samples[i])
 #     counter += 1
 #
 # print(counter)
+for i in tree_leafs_unique:
+    # for j in tree_leafs:
+    #     centroid_lst[i].append(x_train[i])
+    # matches = list(x for x in tree_leafs if x == i)
+    # print(list(tree_leafs).index[i])
+    tmp_lst = []
+    for k, j in enumerate(tree_leafs):
+        if j == i:
+            tmp_lst.append(x_train[k])
+
+    # numpy.asarray(tmp_lst)
+    # print(tmp_lst)
+    avg = 0
+    temp_centroid = numpy.zeros(number_of_features)
+
+    for y in range(number_of_features):
+        counter = 0
+        for j in tmp_lst:
+            avg += j[y]
+            # print(j[y],end=' ')
+            counter += 1
+
+        avg =avg/counter
+        # print()
+        # print(avg)
+        temp_centroid[y] = avg
+        # print(y)
+
+        # print()
+    leaf_centroids_test[i] = temp_centroid
+    # print(temp_centroid)
+    # break
+#
+for i in leaf_centroids_test:
+    print(i , " " , leaf_centroids_test[i])
 
